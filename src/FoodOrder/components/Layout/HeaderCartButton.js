63e4d7d7813471e70e5ea904055b64f8f1eea4b1
Thierry Raeber <cyphics@protonmail.com>
2023-01-30 09:18:33 +0100

    make nice animation when adding to cart

--- src/FoodOrder/components/Layout/HeaderCartButton.js
+++ src/FoodOrder/components/Layout/HeaderCartButton.js
@@ -1,22 +1,37 @@
 import styles from './HeaderCartButton.module.css'
 import CartIcon from "./CartIcon";
-import {Fragment, useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
     const ctx = useContext(CartContext);
-    const numOfCartItems = ctx.items.reduce((current, item) => {
+    const {items} = ctx;
+    const numOfCartItems = items.reduce((current, item) => {
         return current + item.amount;
     }, 0);
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+    const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, [300]);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items])
 
     return (
-      <Fragment>
-          <button onClick={props.onClick} className={styles.button}>
-              <span className={styles.icon}><CartIcon/></span>
-              <span>Your Cart</span>
-              <span className={styles.amount}>{numOfCartItems}</span>
-          </button>
-      </Fragment>
+      <button onClick={props.onClick} className={btnClasses}>
+          <span className={styles.icon}><CartIcon/></span>
+          <span>Your Cart</span>
+          <span className={styles.amount}>{numOfCartItems}</span>
+      </button>
     )
 }
 
