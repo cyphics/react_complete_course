7770fffe124efb0956cc7e87d774af80dd79c500
Thierry Raeber <cyphics@protonmail.com>
2023-01-30 17:56:18 +0100

    use custom hook for newTask as well

--- src/MoreHooks/components/NewTask/NewTask.js
+++ src/MoreHooks/components/NewTask/NewTask.js
@@ -1,50 +1,37 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 import {FIREBASE_URL} from "../../../constants";
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        FIREBASE_URL + 'tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    const {isLoading, error, sendRequest: sendTaskRequest} = useHttp();
+    const createTask = (taskText, taskData) => {
+        const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+        const createdTask = {id: generatedId, text: taskText};
+        props.onAddTask(createdTask);
     }
-    setIsLoading(false);
-  };
 
-  return (
-    <Section>
-      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
-      {error && <p>{error}</p>}
-    </Section>
-  );
+    const enterTaskHandler = async (taskText) => {
+        sendTaskRequest(
+          {
+              url: FIREBASE_URL + 'tasks.json',
+              method: 'POST',
+              body: {text: taskText},
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+          }, createTask.bind(null, taskText)
+        )
+    };
+
+    return (
+      <Section>
+          <TaskForm onEnterTask={enterTaskHandler} loading={isLoading}/>
+          {error && <p>{error}</p>}
+      </Section>
+    );
 };
 
 export default NewTask;
