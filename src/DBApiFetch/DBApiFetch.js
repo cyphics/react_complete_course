d674c23d4075bb0dfb6b149cadf2af9cd3769440
Thierry Raeber <cyphics@protonmail.com>
2023-01-30 09:44:55 +0100

    make UI responsive to different states (loading, no movies, etc)

--- src/DBApiFetch/DBApiFetch.js
+++ src/DBApiFetch/DBApiFetch.js
@@ -5,8 +5,10 @@ import './App.css';
 
 function DBApiFetch() {
     const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     async function fetchMoviesHandler() {
+        setIsLoading(true);
         const response = await fetch('https://swapi.dev/api/films/')
         const data = await response.json();
         const transformedMovies = data.results.map(movieData => {
@@ -18,22 +20,8 @@ function DBApiFetch() {
             }
         });
         setMovies(transformedMovies);
+        setIsLoading(false);
     }
-
-    const dummyMovies = [
-        {
-            id: 1,
-            title: 'Some Dummy Movie',
-            openingText: 'This is the opening text of the movie',
-            releaseDate: '2021-05-18',
-        },
-        {
-            id: 2,
-            title: 'Some Dummy Movie 2',
-            openingText: 'This is the second opening text of the movie',
-            releaseDate: '2021-05-19',
-        },
-    ];
 
     return (
       <React.Fragment>
@@ -41,7 +29,9 @@ function DBApiFetch() {
               <button onClick={fetchMoviesHandler}>Fetch Movies</button>
           </section>
           <section>
-              <MoviesList movies={movies}/>
+              {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
+              {!isLoading && movies.length === 0 && <p> Found no movies.</p>}
+              {isLoading && <p>Loading...</p>}
           </section>
       </React.Fragment>
     );
